39b1afe916e69e12877ec6c9ed6a5827ea49d4b2
Chenguang Wang <wangcg.pku@gmail.com>
2019-04-23 11:21:46 -0700

    fix trial executor

--- python/ray/tune/ray_trial_executor.py
+++ python/ray/tune/ray_trial_executor.py
@@ -312,11 +312,47 @@ class RayTrialExecutor(TrialExecutor):
             raise ValueError("Trial was not running.")
         self._running.pop(trial_future[0])
         with warn_if_slow("fetch_result"):
-            result = ray.get(trial_future[0])
+            # result = ray.get(trial_future[0])
+            result = self.ray_get_and_free(trial_future[0])
 
         # For local mode
         if isinstance(result, _LocalWrapper):
             result = result.unwrap()
+        return result
+
+    _last_free_time = 0.0
+    _to_free = []
+
+    def ray_get_and_free(object_ids):
+        """Call ray.get and then queue the object ids for deletion.
+
+        This function should be used whenever possible in RLlib, to optimize
+        memory usage. The only exception is when an object_id is shared among
+        multiple readers.
+
+        Args:
+            object_ids (ObjectID|List[ObjectID]): Object ids to fetch and free.
+
+        Returns:
+            The result of ray.get(object_ids).
+        """
+
+        global _last_free_time
+        global _to_free
+
+        result = ray.get(object_ids)
+        if type(object_ids) is not list:
+            object_ids = [object_ids]
+        _to_free.extend(object_ids)
+
+        # batch calls to free to reduce overheads
+        now = time.time()
+        if (len(_to_free) > 100
+                or now - _last_free_time > 10.0):
+            ray.internal.free(_to_free)
+            _to_free = []
+            _last_free_time = now
+
         return result
 
     def _commit_resources(self, resources):
